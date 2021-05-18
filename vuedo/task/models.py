98a4fd9065f4027ae6128f29d68fb7106c743b44
mutyaba84 <mutyabadennie@gmail.com>
2021-05-18 16:11:24 +0100

    updated models files

--- vuedo/task/models.py
+++ vuedo/task/models.py
@@ -2,14 +2,13 @@ from django.db import models
 
 
 class Task(models.Model):
-    TODO = 'todo'
-    DONE = 'done'
+    TODO = "todo"
+    DONE = "done"
 
-    STATUS_CHOICES = (
-        (TODO, 'Todo'),
-        (DONE, 'Done')
-    )
+    STATUS_CHOICES = ((TODO, "Todo"), (DONE, "Done"))
 
     description = models.CharField(max_length=255)
-    status = models.CharField(
-        max_length=10, choices=STATUS_CHOICES, default=TODO)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default=TODO)
+
+    def __str__(self):
+        return self.description
