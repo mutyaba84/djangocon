0345fcab1a034c5c506ee9452913edbc4338589d
mutyaba84 <mutyabadennie@gmail.com>
2021-05-19 10:19:03 +0000

    updated version

--- vuedo/vuedo/urls.py
+++ vuedo/vuedo/urls.py
@@ -16,14 +16,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from task.views import TaskViewSet
+from task.views import VariableViewSet
 
 from rest_framework import routers
 
 router = routers.DefaultRouter()
-router.register(r'tasks', TaskViewSet)
+router.register(r"Variable", VariableViewSet)
 
 urlpatterns = [
-    path('', include(router.urls)),
-    path('admin/', admin.site.urls),
+    path("", include(router.urls)),
+    path("admin/", admin.site.urls),
 ]
